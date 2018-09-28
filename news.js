f5eb182296507947b036c5d88c29885960991fc6
Amber <amber.r.struckman@gmail.com>
2018-09-27 20:43:13 -0700

    news ajax

---
+++ news.js
@@ -0,0 +1,9 @@
+var queryURL = 'https://newsapi.org/v2/top-headlines?' +
+'country=us&' +
+'apiKey=5f6bee7ae68446d687e4b4e0c9bbad22';
+
+  // Creating an AJAX 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then();  
