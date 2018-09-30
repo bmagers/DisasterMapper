cbc81cd2404c3045905de57569c80065ddcf9c41
Amber <amber.r.struckman@gmail.com>
2018-09-29 21:28:31 -0700

    connected the form submit to the API

--- fema.js
+++ fema.js
@@ -1,7 +1,64 @@
-var queryURL = "https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries" ///api/open/[version]/[entity]
 
-  // Creating an AJAX 
+ 
+
+$("#add-employee-btn").on("click", function(event) {
+  // Don't refresh the page!
+  event.preventDefault();
+  //"title": "FLOOD",
+  let title = $("#title-input").val().trim().toUpperCase();
+  console.log(title);
+  //"incidentType": "Flood",
+  let incidentType = $("#incident-type-input").val().trim().toLowerCase();
+  console.log(incidentType);
+  //"state": "TX",
+  // let state = $("#state-input").val().trim();
+  //"declaredCountyArea": "",
+  let area = $("#area-input").val().trim();
+  console.log(area);
+    //"incidentBeginDate": "1953-06-19T00:00:00.000Z",
+  let beginDate = $("#begin-input").val().trim();
+  console.log(beginDate);
+  //"incidentBeginDate": "1953-06-19T00:00:00.000Z",
+  let endDate = $("#end-input").val().trim();
+  console.log(endDate);
+  
+  let queryFilter = "";
+  if (title.length > 0) {
+    queryFilter = addFilter(queryFilter, "substringof('" + title + "',title)");
+  }
+  if (incidentType.length > 0) {
+    incidentType = incidentType[0].toUpperCase() + incidentType.slice(1);
+    queryFilter = addFilter(queryFilter, "substringof('" + incidentType + "',incidentType)");
+  }
+  if (area.length > 0) {
+    queryFilter = addFilter(queryFilter, "(substringof('" + area.toUpperCase() + "',state) or substringof('" + area + "',declaredCountyArea))");
+  }
+  if (beginDate.length > 0) {
+    beginDate = new Date(beginDate).toISOString();
+    queryFilter = addFilter(queryFilter, "incidentBeginDate ge '" + beginDate + "'");
+  }
+  if (endDate.length > 0) {
+    endDate = new Date(endDate).toISOString();
+    queryFilter = addFilter(queryFilter, "incidentEndDate le '" + endDate + "'");
+  }
+  let queryURL = "https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries?$filter=" + queryFilter + "&$orderby=incidentBeginDate%20desc&$select=title,incidentType,declaredCountyArea,state,incidentBeginDate,incidentEndDate&$top=10";
+
+  console.log(queryURL);
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then();  
+  }).then(function(response){ 
+    var disasterInfo = response.DisasterDeclarationsSummaries
+    console.log(disasterInfo.length);
+    console.log(disasterInfo);
+  });
+});
+
+function addFilter(filter, newFilter) {
+  if (filter.length > 0) {
+    return filter + " and " + newFilter;
+  }
+  return newFilter;
+}
+  // Creating an AJAX 
+ 
